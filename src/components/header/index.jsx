ba7fa1a059f788f43a4ad7dfb912d3ad54c09836
ndhieu2701 <hieu@v7rx3.onmicrosoft.com>
2023-02-19 00:54:38 +0700

    finish project, responsive

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../../assets/logo.png";
 import "./header.css";
 import { changeThemes } from "../../features/themes/themesSlice";
@@ -17,52 +17,70 @@ const Header = () => {
     }
   });
 
+  //show menu in mobile
+  const [showMenu, setShowMenu] = useState(false);
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <header className={`header ${theme}`}>
       <div className="header-container">
-        <div className="logo">
+        <div className={!showMenu ? "logo" : "logo-disable"}>
           <img src={logo} alt="logo" className="logo-img" />
         </div>
-        <div className="menu">
-          <div
-            className={`multi-themes ${theme}`}
-            onClick={() => dispatch(changeThemes())}
-          >
-            <button className="multi-themes-icon">
-              {theme === "light" ? (
-                <i className="bx bx-brightness"></i>
-              ) : (
-                <i className="bx bxs-moon"></i>
-              )}
-            </button>
-          </div>
+        <div className={!showMenu ? "menu show-menu" : "menu"}>
           <ul className="navbar">
             <li className="navbar-item">
               <a href="#" className={`navbar-item-link ${theme}`}>
+                <i className="bx bx-home-alt"></i>
                 Home
               </a>
             </li>
             <li className="navbar-item">
               <a href="#about" className={`navbar-item-link ${theme}`}>
+                <i className="bx bx-user"></i>
                 About
               </a>
             </li>
             <li className="navbar-item">
               <a href="#skills" className={`navbar-item-link ${theme}`}>
+                <i className="bx bx-file"></i>
                 Skills
               </a>
             </li>
             <li className="navbar-item">
               <a href="#projects" className={`navbar-item-link ${theme}`}>
+                <i className="bx bx-briefcase-alt"></i>
                 Projects
               </a>
             </li>
             <li className="navbar-item">
               <a href="#contact" className={`navbar-item-link ${theme}`}>
+                <i className="uil uil-message"></i>
                 Contact
               </a>
             </li>
+            <div
+              className={`navbar-item multi-themes ${theme}`}
+              onClick={() => dispatch(changeThemes())}
+            >
+              <button className="multi-themes-icon">
+                {theme === "light" ? (
+                  <i className="bx bx-brightness"></i>
+                ) : (
+                  <i className="bx bxs-moon"></i>
+                )}
+              </button>
+            </div>
           </ul>
+          <div className={`menu-icon ${theme}`} onClick={toggleMenu}>
+            {showMenu ? (
+              <i className="bx bx-x"></i>
+            ) : (
+              <i className="uil uil-apps"></i>
+            )}
+          </div>
         </div>
       </div>
     </header>
